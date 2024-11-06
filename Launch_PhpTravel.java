384c40aea43099d9687bd1e30f4e955462d4a494
Shradha <shradha.chinchwade@gmail.com>
2024-11-06 23:03:49 +0530

    Add Launch_PhpTravel.java

---
+++ Launch_PhpTravel.java
@@ -0,0 +1,28 @@
+package phptest;
+import org.testng.Assert;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Launch_PhpTravel 
+{
+	WebDriver driver;
+	@BeforeClass
+	public void setup()
+	{
+		WebDriver driver= new ChromeDriver();
+		driver.manage().window().maximize();
+		
+	}
+	@Test(priority=1)
+	public void openurl()
+	{
+		driver.get("https://www.phptravels.net");
+		String currentUrl = driver.getCurrentUrl();
+        System.out.println("Current URL of new tab: " + currentUrl);
+		 Assert.assertTrue(currentUrl.contains("https://www.phptravels.net"));
+
+	}
+
+}
