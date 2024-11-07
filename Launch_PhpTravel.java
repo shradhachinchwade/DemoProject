7b8fcb5c3d17e78825ff98d23fede66ce2e396ba
Shradha <shradha.chinchwade@gmail.com>
2024-11-07 22:29:59 +0530

    Launch_PhpTravel.java this is first commit

--- Launch_PhpTravel.java
+++ Launch_PhpTravel.java
@@ -8,20 +8,20 @@ import org.openqa.selenium.chrome.ChromeDriver;
 public class Launch_PhpTravel 
 {
 	WebDriver driver;
+	
 	@BeforeClass
 	public void setup()
 	{
-		WebDriver driver= new ChromeDriver();
-		driver.manage().window().maximize();
-		
+		driver= new ChromeDriver();
+	    driver.manage().window().maximize();
 	}
 	@Test(priority=1)
 	public void openurl()
 	{
-		driver.get("https://www.phptravels.net");
+		driver.get("https://www.phptravels.net/");
 		String currentUrl = driver.getCurrentUrl();
         System.out.println("Current URL of new tab: " + currentUrl);
-		 Assert.assertTrue(currentUrl.contains("https://www.phptravels.net"));
+		Assert.assertTrue(currentUrl.contains("https://www.phptravels.net/"));
 
 	}
 
